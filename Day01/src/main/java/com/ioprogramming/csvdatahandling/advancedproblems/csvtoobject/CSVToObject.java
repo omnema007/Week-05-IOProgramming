2c5dd64a1558d12de46747389f42e9aaa6ad1cfb
omnema007 <nemaom2003@gmail.com>
2025-02-19 11:17:47 +0530

    [Om] Add. Solution of Convert CSV Data into Java Objects

---
+++ Day01/src/main/java/com/ioprogramming/csvdatahandling/advancedproblems/csvtoobject/CSVToObject.java
@@ -0,0 +1,36 @@
+package com.ioprogramming.csvdatahandling.advancedproblems.csvtoobject;
+
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVToObject {
+    public static void main(String[] args) {
+        String filePath = "student.csv";
+        List<Student> studentList = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            List<String[]> records = reader.readAll();
+
+            for (int i = 1; i < records.size(); i++) {
+                String[] row = records.get(i);
+                String name = row[0];
+                int age = Integer.parseInt(row[1]);
+                String course = row[2];
+                double marks = Double.parseDouble(row[3]);
+
+                Student student = new Student(name, age, course, marks);
+                studentList.add(student);
+            }
+
+            System.out.println("List of Students:");
+            for (Student student : studentList) {
+                System.out.println(student);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
