0fe812d8cb4e92966b6667a0ee419f7ca7edfd43
omnema007 <nemaom2003@gmail.com>
2025-02-20 10:35:31 +0530

    [Om] Add. Solution of  Filter JSON data: Print only users older than 25 years.

---
+++ Day02/src/main/java/com/ioprogramming/json/handsonpracticeproblems/filterjsondata/ParseAndFilterJson.java
@@ -0,0 +1,37 @@
+package com.ioprogramming.json.handsonpracticeproblems.filterjsondata;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParseAndFilterJson {
+    public static void main(String[] args) {
+        String json = "[{\"name\": \"Alice\", \"age\": 25}, " +
+                        "{\"name\": \"Bob\", \"age\": 30}, " +
+                        "{\"name\": \"Charlie\", \"age\": 28}, " +
+                        "{\"name\": \"David\", \"age\": 22}]";
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(json);
+
+            List<JsonNode> filteredRecords = new ArrayList<>();
+
+            for (JsonNode node : rootNode) {
+                if (node.get("age").asInt() > 25) {
+                    filteredRecords.add(node);
+                }
+            }
+
+
+            String filteredJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(filteredRecords);
+            System.out.println(filteredJson);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
